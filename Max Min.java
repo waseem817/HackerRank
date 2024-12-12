7799b01fd0efe1c5076cdad05268682b031f3932
waseem817 <s.shoaibwaseem786@gmail.com>
2024-12-12 14:05:03 +0530

    Create Max Min.java

---
+++ Max Min.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    public static int maxMin(int k, List<Integer> arr) {
+    long min,max,s=0;
+    Collections.sort(arr);
+    int n=arr.size();
+    for(int i=0; i<n;i++)
+    s+=arr.get(i);
+    System.out.println((s-arr.get(n-1))+" "+(s-arr.get(0)));
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int k = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(bufferedReader.readLine().trim());
+            arr.add(arrItem);
+        }
+
+        int result = Result.maxMin(k, arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
